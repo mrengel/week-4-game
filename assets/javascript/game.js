378ae7d3fcf5ead8c7d2b783cbc946ddb88a3358
mrengel <m.r.engel2010@gmail.com>
2017-08-09 22:52:50 -0400

    Created objects for characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,59 +3,60 @@ $(document).ready(function() {
 	//alert("You must be Batman");
 	//alert("You must face the Joker");
 
-	var firstCharHealth=100;
-	var secondCharHealth=120;
+	var yourCharacter=[];
+		//name: " ",
+		//healthPoints: null,
+		//attackPower: null,
+		//counterAttackPower: null,
+	//}
+	var defender=[];
 
-	var characterArrayList=[
-
-		{
-			"Name"="Batman",
-			healthPoints = 120,
-			attackPower = 10,
-			counterAttackPower = 10,
-		},
-
-		{
-			"Name"="Joker",
-			healthPoints = 130,
-			attackPower = 8,
-			counterAttackPower = 8,
-		},
-
-		{
-			"Name"="TwoFace",
-			healthPoints = 110,
-			attackPower = 12,
-			counterAttackPower = 6,
-		},
-
-		{
-			"Name"="MrFreeze",
-			healthPoints = 110,
-			attackPower = 12,
-			counterAttackPower = 6,
-
+	var batman = {
+			name: "Batman",
+			healthPoints: 120,
+			attackPower: 10,
+			counterAttackPower: 10,
+		}
+	var joker ={
+			name: "Joker",
+			healthPoints: 130,
+			attackPower: 8,
+			counterAttackPower: 8,
+		}
+	var twoFace ={
+			name: "TwoFace",
+			healthPoints: 110,
+			attackPower: 12,
+			counterAttackPower: 6,
+		}
+	var mrFreeze ={
+			name: "MrFreeze",
+			healthPoints: 110,
+			attackPower: 12,
+			counterAttackPower: 6,
 		}
 
+		
 
 
+		
 
+	$("#Health1").html("Health: "+batman["healthPoints"]);
+	$("#Health2").html("Health: "+joker["healthPoints"]);
+	$("#Health3").html("Health: "+twoFace["healthPoints"]);
+	$("#Health4").html("Health: "+mrFreeze["healthPoints"]);
 
-
-
-
-	]
-
-	$("#Health1").html("Health: "+firstCharHealth);
-	$("#Health2").html("Health: "+secondCharHealth);
+	console.log(batman["healthPoints"]);
 
 	var newDiv = $("#Batman")
 
 	$("#Batman").on("click",function(){
 
-		console.log(1);
+		console.log(1)
 		
 		$(".Your-Character").append(newDiv);
+		yourCharacter.push.apply(yourCharacter, batman);
+		console.log(this.yourCharacter["healthPoints"]);
 	});
 
 	var newDiv2 = $("#Joker")
@@ -65,27 +66,28 @@ $(document).ready(function() {
 		console.log(1);
 		
 		$(".Defender").append(newDiv2);
+		defender.push(joker);
+		console.log(this.defender["healthPoints"]);
 	});
 
-	function attack(){
-		firstCharHealth = firstCharHealth - 10;
-		secondCharHealth = secondCharHealth - 15;
-		$("#Health1").html("Health: "+firstCharHealth);
-		$("#Health2").html("Health: "+secondCharHealth);
-		console.log(firstCharHealth);
-		console.log(secondCharHealth);
-	}
+	//function attack(){
+		//firstCharHealth = firstCharHealth - 10;
+		//secondCharHealth = secondCharHealth - 15;
+		//$("#Health1").html("Health: "+firstCharHealth);
+		//$("#Health2").html("Health: "+secondCharHealth);
+		//console.log(firstCharHealth);
+		//console.log(secondCharHealth);
+	//}
 
-	$(".Attack-Button").on("click",function(){
-		attack();
-		if(secondCharHealth===0){
-			alert("I am the night...");
-			alert("I am vengeance...");
-			alert("I am BATMAN!");
-		}
+	//$(".Attack-Button").on("click",function(){
+		//attack();
+		//if(secondCharHealth===0){
+			//alert("I am the night...");
+			//alert("I am vengeance...");
+			//alert("I am BATMAN!");
+		//}
 
 	});
 
 
 
-});
